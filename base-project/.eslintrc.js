4ebf9ec702422311869b0163b62e2e2d18e96fe6
David <david_borelli@live.com>
2020-05-19 22:16:27 -0300

    Adicionando o ESLint

---
+++ base-project/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
