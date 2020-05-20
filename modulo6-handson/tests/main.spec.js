c5e0cca048879df70cf21512d7944b1a0226a3e2
David <david_borelli@live.com>
2020-05-20 00:50:52 -0300

    Criando testes de uma calculadora

--- modulo6-handson/tests/main.spec.js
+++ modulo6-handson/tests/main.spec.js
@@ -31,4 +31,36 @@ describe("calc", function () {
       expect(calc.div).to.be.a("function");
     });
   });
+
+  describe("Sum", function () {
+    it("should return 4 when `sum(2,2)`", function () {
+      expect(calc.sum(2, 2)).to.be.equal(4);
+    });
+  });
+
+  describe("Sub", function () {
+    it("should return 4 when `sub(6,2)`", function () {
+      expect(calc.sub(6, 2)).to.be.equal(4);
+    });
+
+    it("should return -4 when `sub(2,6)`", function () {
+      expect(calc.sub(2, 6)).to.be.equal(-4);
+    });
+  });
+
+  describe("Mult", function () {
+    it("should return 4 when `mult(2,2)`", function () {
+      expect(calc.mult(2, 2)).to.be.equal(4);
+    });
+  });
+
+  describe("Div", function () {
+    it("should return 4 when `div(8,2)`", function () {
+      expect(calc.div(8, 2)).to.be.equal(4);
+    });
+
+    it("should return `Não é possível divisão por zero!` when divide by 0", function () {
+      expect(calc.div(8, 0)).to.be.equal("Não é possível divisão por zero!");
+    });
+  });
 });
